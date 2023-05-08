3f4a8c62438e48f17d3f81d977eb6cc6aa35cdce
Andychristoff <andychristoff@gmail.com>
2023-05-08 20:55:28 -0300

    terminado

--- src/Library/Iprinter.cs
+++ src/Library/Iprinter.cs
@@ -5,3 +5,8 @@ namespace Full_GRASP_And_SOLID.Library
     void PrintRecipe(Recipe recipe);
     }
 }
+
+// El ejercicio no cumplia con el principio de polimorfismo ya que utilizaba logica para preguntar el destino del texto
+// y asi elegir un metodo de impresion para utilizar, todo dentro de la misma clase. Esto lo arregle usando una interfaz 
+// dividiendo la tarea en 2 clases distintas, una clase que emplearia el metodo cuando el texto se dirije a una impresora,
+// y otra clase que emplearia el metodo cuando el texto se dirije a una consola.
